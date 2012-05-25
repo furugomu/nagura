91765aacfa870274186292cf0016506ef1042eb8
furugomu <furugomu@gmail.com>
2012-05-25 15:48:23 +0900

    モゲ～

---
+++ script/load_tsv.rb
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+ENV["RAILS_ENV"] ||= 'development'
+require File.expand_path('../../config/application',  __FILE__)
+Rails.application.require_environment!
+
+USAGE = '標準入力にスプレッドシートの中身を貼り付けろ'
+
+def main
+  Rank.cache do
+    Dojo.transaction do
+      $stdin.each_line do |line|
+        begin
+          Dojo.new_from_tsv(line).save!
+        rescue =>e
+          $stderr.puts(line)
+          raise e
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  main
+end
