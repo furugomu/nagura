4bfb05a3f09719955e6adf3abe9594ec977b35c0
furugomu <furugomu@gmail.com>
2012-05-25 11:54:37 +0900

    hajimemashite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nagura::Application.config.secret_token = '9866374f3f01f0ca3d96a585fa2ca6ce82646db310bdd5a56d0cc8feffb288a2133c54f760dbb313143a3076b46716b7979186d821d6a19eb39a432375097ebb'
